639a637e53fac87a58cfdc9e1f0b9ea701a939a3
Alex Bloom <alexbloomdesign@gmail.com>
2014-02-11 12:52:51 -0500

    Adding Breadcrumb nav to Sub Pages

---
+++ interactive-landscape.php
@@ -0,0 +1,175 @@
+<?php    $section = 'index'; include_once 'header.php' ?>
+
+<!-- TFI New Media Fund Page -->
+
+<section id="" class="">
+
+	<nav class="breadcrumb">
+		<ul>
+			<li> <a href="tfi-new-media-fund.php"> Interactive Landscape </a> </li>
+		</ul>
+	</nav>
+	
+	<nav class="subnav clearfix" id""> 	
+		<dl class="subnav clearfix">
+			<dt>Type:</dt>
+			<dd class="active"><a href="#">Web</a></dd>
+			<dd><a href="#">App</a></dd>
+			<dd><a href="#">Installation</a></dd>
+			<dd><a href="#">Other</a></dd>
+		</dl>	
+	</nav>	
+	
+	<h2 class="projects-title">Web Based Projects</h2>
+	<ul class="projects" id="">
+	<li class="project clearfix" id="project-1">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/il-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<small>November 2013</small>
+					<h2>Emotional Arcade <span class="expand-icon"> &darr; </span> </h2>
+				</div>
+					<a class="project-url" href="http://emotionalarcade.com"target=_blank>emotionalarcade.com &rarr;</a>			
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					The Emotional Arcade engages technology to allow people to play competitive games using their own emotions. The Arcade is an attempt to provide at least one space on the planet where unrestrained emotional outbursts are honored, and celebrated.
+				</p>
+				<p>
+					What are we if not our emotions? In a world that increasingly limits sponteneous emotional expression, the installation creates a space for personal revelation. It’s  a chance to explore the patterns of feeling in a fun way. And everyone who plays gets a lollypop.
+				</p>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-2">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<small>Launch Date</small>
+					<h2>Project Name <span class="expand-icon"> &darr; </span> </h2>
+				</div>
+					<a class="project-url" href="#"target=_blank>projecturl.com &rarr;</a>			
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					The Emotional Arcade engages technology to allow people to play competitive games using their own emotions.  The Arcade is an attempt to provide at least one space on the planet where unrestrained emotional outbursts are honored, and celebrated.
+				</p>
+				<p>
+					What are we if not our emotions? In a world that increasingly limits sponteneous emotional expression, the installation creates a space for personal revelation. It’s  a chance to explore the patterns of feeling in a fun way. And everyone who plays gets a lollypop.
+				</p>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-3">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<small>Launch Date</small>
+					<h2>Project Name <span class="expand-icon"> &darr; </span> </h2>
+				</div>
+					<a class="project-url" href="#"target=_blank>projecturl.com &rarr;</a>			
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					The Emotional Arcade engages technology to allow people to play competitive games using their own emotions.  The Arcade is an attempt to provide at least one space on the planet where unrestrained emotional outbursts are honored, and celebrated.
+				</p>
+				<p>
+					What are we if not our emotions? In a world that increasingly limits sponteneous emotional expression, the installation creates a space for personal revelation. It’s  a chance to explore the patterns of feeling in a fun way. And everyone who plays gets a lollypop.
+				</p>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-4">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<small>Launch Date</small>
+					<h2>Project Name <span class="expand-icon"> &darr; </span> </h2>
+				</div>
+					<a class="project-url" href="#"target=_blank>projecturl.com &rarr;</a>			
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					The Emotional Arcade engages technology to allow people to play competitive games using their own emotions.  The Arcade is an attempt to provide at least one space on the planet where unrestrained emotional outbursts are honored, and celebrated.
+				</p>
+				<p>
+					What are we if not our emotions? In a world that increasingly limits sponteneous emotional expression, the installation creates a space for personal revelation. It’s  a chance to explore the patterns of feeling in a fun way. And everyone who plays gets a lollypop.
+				</p>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-5">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<small>Launch Date</small>
+					<h2>Project Name <span class="expand-icon"> &darr; </span> </h2>
+				</div>
+					<a class="project-url" href="#"target=_blank>projecturl.com &rarr;</a>			
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					The Emotional Arcade engages technology to allow people to play competitive games using their own emotions.  The Arcade is an attempt to provide at least one space on the planet where unrestrained emotional outbursts are honored, and celebrated.
+				</p>
+				<p>
+					What are we if not our emotions? In a world that increasingly limits sponteneous emotional expression, the installation creates a space for personal revelation. It’s  a chance to explore the patterns of feeling in a fun way. And everyone who plays gets a lollypop.
+				</p>
+			
+			</section>
+		
+		</article>
+	</li>
+	</ul>
+	
+	<nav class="subnav clearfix" id""> 	
+		<dl class="subnav clearfix">
+			<dt>Explore type:</dt>
+			<dd><a href="#">App</a></dd>
+			<dd><a href="#">Installation</a></dd>
+			<dd><a href="#">Other</a></dd>
+		</dl>	
+	</nav>
+	
+</section>
+
+<!-- End Project Page -->
+    
+<?php include_once 'footer.php' ?>
---
+++ tfi-new-media-fund.php
@@ -0,0 +1,409 @@
+<?php include_once 'header.php' ?>
+
+<!-- TFI New Media Fund Page -->
+
+<section id="" class="">
+	<nav class="breadcrumb">
+		<ul>
+			<li> <a href="tfi-new-media-fund.php"> TFI New Media Fund </a> </li>
+		</ul>
+	</nav>
+	
+	<nav class="subnav clearfix" id""> 	
+		<dl class="subnav clearfix">
+			<dt>Year:</dt>
+			<dd class="active"><a href="#">2014</a></dd>
+			<dd><a href="#">2013</a></dd>
+			<dd><a href="#">2012</a></dd>
+		</dl>
+	</nav>	
+	
+	<nav class="scrollnav">
+		<ul>
+			<li> <a class="scrollnav-link" id="scrollnav-1" href="#project-1"> </a> </li>
+			<li> <a class="scrollnav-link" id="scrollnav-2" href="#project-2"> </a> </li>
+			<li> <a class="scrollnav-link" id="scrollnav-3" href="#project-3"> </a> </li>
+			<li> <a class="scrollnav-link" id="scrollnav-4" href="#project-4"> </a> </li>
+			<li> <a class="scrollnav-link" id="scrollnav-5" href="#project-5"> </a> </li>
+			<li> <a class="scrollnav-link" id="scrollnav-6" href="#project-6"> </a> </li>		
+		</ul>	
+	</nav>
+		
+	<h2 class="projects-title">Funded Projects</h2>
+	<ul class="projects" id="">
+	<li class="project clearfix" id="project-1">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-1">American Promise <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">American Promise URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team clearfix">
+					<li><strong>Natasha Logan</strong> <br> Transmedia Producer </li>
+					<li><strong>Firstname Lastname</strong> <br> Title</li>
+					<li><strong>Firstname Lastname</strong> <br> Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-2">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-2">Project 2 Title <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">Project 2 URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team">
+					<li>Natasha Logan — Transmedia Producer </li>
+					<li>Firstname Lastname - Title</li>
+					<li>Firstname Lastname - Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-3">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-3">Project 3 Title <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">Project 3 URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team">
+					<li>Natasha Logan — Transmedia Producer </li>
+					<li>Firstname Lastname - Title</li>
+					<li>Firstname Lastname - Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-4">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-4">Project 4 Title <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">Project 4 URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team">
+					<li>Natasha Logan — Transmedia Producer </li>
+					<li>Firstname Lastname - Title</li>
+					<li>Firstname Lastname - Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-5">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-5">Project 5 Title <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">Project 5 URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team">
+					<li>Natasha Logan — Transmedia Producer </li>
+					<li>Firstname Lastname - Title</li>
+					<li>Firstname Lastname - Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>
+	<li class="project clearfix" id="project-6">
+		<article>
+
+			<header class="project-header" id="">
+				<img src="img/content/prototype/project-image.jpg" alt="Project Image">
+				<div class="overlay-wrap">
+					<div class="floating-text">
+					<h2><a class="local-link" href="#project-6">Project 6 Title <span class="expand-icon"> &darr; </span> </a></h2>
+				</div>
+					<a class="project-url" href="#">Project 6 URL &rarr;</a>
+								
+				</div>
+			</header>
+			
+			<section class="project-content">
+
+				<p class="intro">
+					American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.
+				</p>
+				<p>
+					To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+				
+				<p>American Promise follows filmmaker-parents who spend 12 years with the camera turned on themselves and another African American family as their firstborn sons enter a prestigious college preparatory school in 1999. An intimate, poignant and complex portrayal of how race and privilege are experienced by African American middle class families today.</p>
+			
+				<p>To further the work of the film, the team is creating a web series and a goal-setting mobile app called Promise Tracker, which is supported by the TFI New Media Fund.</p>
+			
+				<h3>Team Leaders</h3>
+				<ul class="team-leaders clearfix">
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+						<span class="name"> <strong> Michele Stephenson</strong> <br> Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Joe Brewster</strong>  <br>  Director, Producer </span>
+					</li>
+					<li>
+						
+						<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+						<span class="name"> <strong> Another Leader</strong> <br>  Director, Producer </span>
+					</li>
+				</ul>		
+
+				<h3>Core Team</h3>
+				<ul class="core-team">
+					<li>Natasha Logan — Transmedia Producer </li>
+					<li>Firstname Lastname - Title</li>
+					<li>Firstname Lastname - Title</li>
+				</ul>
+			
+			</section>
+		
+		</article>
+	</li>	
+	</ul>
+	
+	<section class="jury" id="">
+		<h2>Meet The Jury</h2>
+		<ul class="jury-members clearfix">
+			<li>
+				<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+				<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
+			</li>
+			<li>
+				<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+				<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
+			</li>
+			<li>
+				<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+				<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+			</li>
+			<li>
+				<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+				<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+			</li>
+		</ul>
+		
+
+	</section>
+	
+	
+	<nav class="subnav clearfix" id""> 	
+		<dl class="subnav clearfix">
+			<dt>View another year:</dt>
+			<dd><a href="#">2013</a></dd>
+			<dd><a href="#">2012</a></dd>
+		</dl>	
+	</nav>
+	
+</section>
+
+<!-- End Project Page -->
+    
+<?php include_once 'footer.php' ?>
