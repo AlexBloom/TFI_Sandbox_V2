edd89fafb7fa0d2274a96cf78f8ac762e5a28a1f
Alex Bloom <alexbloomdesign@gmail.com>
2014-02-24 17:38:13 -0800

    List Edits

---
+++ funding.php
@@ -0,0 +1,38 @@
+<?php include_once 'inc/head.inc' ?>
+
+<!-- Custom Meta Info Here -->
+<link rel="canonical" href="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="TFI New Media Fund - 2014 Funded Projects - TFI Sandbox" />
+<meta property="og:description" content="The TFI New Media Fund is a grant for filmmakers pushing the boundaries of traditional filmmaking and documentary work." />
+<meta property="og:url" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:site_name" content="TFI Sanxbox" />
+<meta property="article:published_time" content="2014-02-04T21:43:55+00:00" />
+<meta property="article:modified_time" content="2014-02-07T19:41:37+00:00" />
+<meta property="og:image" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014/" />
+
+<?php include_once 'inc/header.inc' ?>
+
+<!-- Index Page -->
+<section id="main">
+	<section>
+		<article>
+			<header>
+				<section class="content">
+				<h1>Funding Timeline</h1>
+				<p class="intro"> Funding Opportunities throughout the year.
+				</p>
+				</section>
+			</header>	
+			<section class="content">
+				
+			</section>
+		</article>
+	
+
+	</section>
+</section>	
+<!-- End Index Page --> 
+	 
+<?php include_once 'inc/footer.inc' ?>
---
+++ project-management.php
@@ -0,0 +1,82 @@
+<?php include_once 'inc/head.inc' ?>
+
+<!-- Custom Meta Info Here -->
+<link rel="canonical" href="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="TFI New Media Fund - 2014 Funded Projects - TFI Sandbox" />
+<meta property="og:description" content="The TFI New Media Fund is a grant for filmmakers pushing the boundaries of traditional filmmaking and documentary work." />
+<meta property="og:url" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:site_name" content="TFI Sanxbox" />
+<meta property="article:published_time" content="2014-02-04T21:43:55+00:00" />
+<meta property="article:modified_time" content="2014-02-07T19:41:37+00:00" />
+<meta property="og:image" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014/" />
+
+<?php include_once 'inc/header.inc' ?>
+
+<!-- Index Page -->
+<section id="main">
+	<section>
+		<article>
+			<header>
+				<section class="content">
+				<h1>Project Management Guide</h1>
+				<p class="intro"> A reference guide to help you effectively plan, build, and launch a successful new media projects. Each section contains a quick-read guide and includes case studies from seasoned veterans.
+				</p>
+				</section>
+			</header>	
+			<section class="content">
+				<ol class="four-columns clearfix" id="projectmanagement-callouts">
+					<li>
+						<h2>01.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Building a Team</h5>
+						<p>Assembling the cats you need to succeed</p>
+					</li>
+					<li>
+						<h2>02.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Scoping</h5>
+						<p>Describe project goals and establish your level of effort</p>
+						
+					</li>
+					<li>
+						<h2>03.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Timeline</h5>
+						<p>Schedule milestones based on scoping</p>
+					</li>
+					<li>
+						<h2>04.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Budget</h5>
+						<p>Implement project finances</p>	
+					</li>
+					<li>
+						<h2>05.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Distribution</h5>
+						<p>Getting your project to its audience</p>	
+					</li>
+					<li>
+						<h2>06.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>Sustainability</h5>
+						<p>Supporting the lifespan of your project</p>	
+					</li>
+					<li>
+						<h2>07.</h2>
+						<span class="icon-pm icon-"></span>
+						<h5>PR / Marketing</h5>
+						<p>Making the public aware of your project</p>	
+					</li>
+				</ol>
+			</section>
+		</article>
+	
+
+	</section>
+<section>	
+<!-- End Index Page --> 
+	 
+<?php include_once 'inc/footer.inc' ?>
