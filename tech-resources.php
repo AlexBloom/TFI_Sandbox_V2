2e88fed9c3347f5b82bf4addfd2f482045132b5b
Alex Bloom <alexbloomdesign@gmail.com>
2014-09-02 17:53:38 -0400

    Tweaks to TFI NMF Templates

---
+++ tech-resources.php
@@ -0,0 +1,175 @@
+<?php include_once 'inc/head.inc' ?>
+
+<!-- Custom Meta Info Here -->
+<link rel="canonical" href="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="TFI New Media Fund - 2014 Funded Projects - TFI Sandbox" />
+<meta property="og:description" content="The TFI New Media Fund is a grant for filmmakers pushing the boundaries of traditional filmmaking and documentary work." />
+<meta property="og:url" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:site_name" content="TFI Sanxbox" />
+<meta property="article:published_time" content="2014-02-04T21:43:55+00:00" />
+<meta property="article:modified_time" content="2014-02-07T19:41:37+00:00" />
+<meta property="og:image" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014/" />
+
+<?php include_once 'inc/header.inc' ?>
+
+<!-- Index Page -->
+<section id="main" class="clearfix">
+	<section class="clearfix">
+		<article class="clearfix">
+			<header>
+				<section class="content">
+				<h1>Tech Resources</h1>
+				<p class="intro"> Code, platforms, and design help.
+				</p>
+				</section>
+			</header>
+		<div class="left-column clearfix">
+			<nav class="sticky">
+				<h4>Sections</h4>	
+				<ol class="clearfix">
+					<li><a href="#TFIResources">Tools and Platforms <br> <em>Tribeca Film Institute</em></a></li>
+					<li><a href="#ABloomResources">Code Resources <br> <em>Alex Bloom</em> </a></li>
+				</ol>
+			</nav>
+			&nbsp;
+		</div>
+		<div class="right-column clearfix">
+			<section class="content" id="TFIResources">
+				<h2>Tools and Platforms</h2>
+				<p>Recommended by <strong>Tribeca Film Institute</strong></p>
+				<ul>
+					<li>	
+						<h3>Zeega</h3>
+						Zeega is an open-source HTML5 platform for creating interactive documentaries and inventing new forms of storytelling
+						<a href="http://alpha.zeega.org"target=_blank> alpha.zeega.org </a>
+					</li>
+					<li>	
+						<h3>Mozilla PopcornMaker</h3>
+						Making it easy to enhance, remix and share web video
+						<a href="http://mozillapopcorn.org"target=_blank> mozillapopcorn.org </a>
+					</li>
+					<li>	
+						<h3>Storyplanet</h3>
+						Tool to create interactive projects
+						<a href="http://storyplanet.com/index"target=_blank> storyplanet.com/index </a>
+					</li>
+					<li>
+						<h3>Racontr</h3>
+						Create immersive stories without code.
+						<a href="https://beta.racontr.com/"target=_blank> beta.racontr.com </a>
+					</li>
+					<li>
+						<h3>Kaltura</h3>
+						An Open Source Community providing tools such as: online video player, open source editor and open source video for websites.
+						<a href="http://corp.kaltura.com/"target=_blank> corp.kaltura.com </a>
+					</li>
+				</ul>
+			</section>
+			<section class="content" id="ABloomResources">
+				<h2>Code Resources</h2>
+				<p>Recommended by <strong>Alex Bloom</strong> Interaction Designer</p>
+
+				<h3>Design Tools / Interface Prototyping / Before Code</h3>
+				<ul>	
+					<li>	Sketch - An affordable alternative to Adobe Creative Suite - http://bohemiancoding.com/sketch/
+					</li>
+					<li>	Invision - http://www.invisionapp.com/
+					</li>
+				</ul>
+				
+				<h3>Development Tools / Environments / Text Editors</h3>
+				<ul>	
+					<li>	TextMate - http://macromates.com/
+					</li>
+					<li>	Sublime Text - http://www.sublimetext.com/
+					</li>
+					<li>	Coda - https://panic.com/coda/
+					</li>
+					<li>	GitHub &amp; Github for Mac - https://github.com/
+					</li>
+					<li>	Code Kit is my compiler and error checker. It's always running whenever I'm writing code - https://incident57.com/codekit/
+					</li>
+					<li>	Transmit is my file transfer and server management software - http://panic.com/transmit/
+					</li>
+				</ul>
+				
+				<h3>CODE 101 - Libraries, Resources, &amp; Instruction for Code</h3>
+				<ul>	
+					<li>	Initalizr - Virtually Every one of my web projects starts with this. - http://www.initializr.com/
+					</li>
+					<li>	Dochub - A great search and reference sheet for when you forget what something means - http://dochub.io/
+					</li>
+					<li>	Pure CSS - Grab and Go responsive code patterns - http://purecss.io/
+					</li>
+					<li>	IcoMoon Icon Font Generator - http://icomoon.io/
+					</li>
+					<li>	Fontello - Icon Font Generator - http://fontello.com/
+					</li>
+					<li>	Google Fonts Free Webfont Resource - http://www.google.com/fonts/#
+					</li>
+					<li>	Typekit Affordable Webfont Library - http://www.typekit.com
+					</li>
+					<li>	SASS and LESS - Once someone has an understanding of CSS, these are invaluable for making development easier and faster: http://sass-lang.com/ , and http://lesscss.org/
+					</li>
+					<li>	Modernizr - A javascript library to help improve visual consistency across browsers - http://modernizr.com/
+				Web -  Educational / Social Coding Sites
+					</li>
+					<li>	CodePen and JS Fiddle - Quickly test things in the browser and share them for help or advice, great forums - http://codepen.io/ and  http://jsfiddle.net/
+					</li>
+					<li>	Code Academy - Free Online School - http://www.codecademy.com/
+					</li>
+					<li>	Code School &amp; TreeHouse - Affordable Online Schools - https://www.codeschool.com/ and http://teamtreehouse.com/
+					</li>
+				</ul>
+				
+				
+				<h3>Web - Back End Development </h3>
+				<ul>
+					<li>	PHP - Possibly the most well established server side language - http://www.php.net/
+					</li>
+					<li>	Ruby on Rails - Quick learning curve, extremely popular with web based applications - http://rubyonrails.org/
+					</li>
+					<li>	Jekyll - Static Site generator that runs on ruby, stable, secure, and fast - http://jekyllrb.com/
+					</li>
+					<li>	Liquid - Shopify and Siteleaf use this templating language, very easy to learn the syntax - http://liquidmarkup.org/
+					</li>
+				</ul>
+				
+				<h3>Web - Content Management Systems</h3>
+				<ul>
+					<li>	Wordpress - The Old Standard PHP / Database CMS, The largest community of open source developers on the web - http://wordpress.org
+					</li>
+					<li>	Kirby - PHP / File Based CMS - Can run out of dropbox - http://getkirby.com/
+					</li>
+					<li>	SiteLeaf - Jekyll Based CMS - Affordable plans, easily customizable - http://www.siteleaf.com/
+					</li>
+				</ul>
+				
+				<h3>Data Visualization / Multi-Component / Projection Mapping Software</h3>
+				<ul>
+					<li>	Processing - http://www.processing.org/
+					</li>
+					<li>	Cinder Frameworks - http://libcinder.org/
+					</li>
+				</ul>
+				<h3>DIY / Multi-Component / Projection Mapping Hardware </h3>
+				<ul>
+					</li>
+					<li>	Arduino - Awesome open source computer platform & hardware - http://www.arduino.cc/
+					</li>
+					<li>	Brightsign - Good, affordable media players -  http://www.brightsign.biz/
+					</li>
+				</ul>
+				
+				
+			</section>
+		</div>
+		</article>
+
+	</section>
+<section>	
+<!-- End Index Page --> 
+	 
+<?php include_once 'inc/footer.inc' ?>
--- tfi-new-media-fund-2014.php
+++ tfi-new-media-fund-2014.php
@@ -45,72 +45,6 @@
 	<section class="clearfix" id="2014">		
 		
 		<ul class="projects">
-			<li class="project anchor clearfix" id="test-project">
-				<article>
-					<header class="project-header" id="">
-						<a class="local-link" href="#test-project">
-							<div class="project-image-wrap">
-								<img src="img/content/prototype/18days.jpg" alt="Project Image">
-							</div>
-							<div class="header-info">
-								<div class="floating-text">
-								<h2>Title of Project</h2>
-								<p> Project logline / description of project. </p>			
-								</div>
-							</div>
-						</a>
-						<a class="project-url" href="#">Project URL &rarr;</a>				
-					</header>
-					<section class="project-content">
-
-						<p>Project Text</p>
-					
-						<strong>Questions </strong>
-
-						<p>Answers and such.</p>
-					
-						<h3>Team Leaders</h3>
-						<ul class="team-leaders clearfix">
-							<li>
-								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-								</div>
-								<div class="person-info">
-									<h4 class="name"> 
-										 <strong>JIGAR MEHTA</strong> 
-										 <br>
-										 Director/Producer/CO-Creator
-									</h4>
-									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University,...</p>
-								</div>
-								
-							</li>
-							<li>
-								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-								</div>
-								<div class="person-info">
-									<h4 class="name"> 
-										 <strong>YASMIN ELAYAT </strong> 
-										 <br>
-										 TECHNOLOGIST/CO-CREATOR
-									</h4>
-									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction...</p>
-								</div>
-								
-							</li>
-						</ul>
-					
-						<h3>Additional Credits</h3>
-						<ul>
-							<li> <strong> Firstname Lastname </strong> Role and/or Responsibilities </li>
-							<li> <strong> Firstname Lastname </strong> Role and/or Responsibilities </li>
-							<li> <strong> Firstname Lastname </strong> Role and/or Responsibilities </li>
-						</ul>
-			
-					</section>
-				</article>
-			</li>
 			<li class="project anchor clearfix" id="18Days">
 				<article>
 					<header class="project-header" id="">
@@ -128,13 +62,8 @@
 						</a>
 						<a class="project-url" href="http://18DaysinEgypt.com"target=_blank>18DaysinEgypt.com &rarr;</a>				
 					</header>
-					<section class="project-content">
-
-						<p>Project Text</p>
 					
-						<strong>Questions </strong>
-
-						<p>Answers and such.</p>
+					<section class="project-content">
 					
 						<h3>Team Leaders</h3>
 						<ul class="team-leaders clearfix">
@@ -148,7 +77,7 @@
 										 <br>
 										 Director/Producer/CO-Creator
 									</h4>
-									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University,...</p>
+									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University, where he worked on developing tools for collaborative journalism.</p>
 								</div>
 								
 							</li>
@@ -162,20 +91,144 @@
 										 <br>
 										 TECHNOLOGIST/CO-CREATOR
 									</h4>
-									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction...</p>
+									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction designer and software developer based in Cairo.</p>
 								</div>
 								
 							</li>
 						</ul>		
-
 						
-					
+						<h3>Project Update</h3>
+						<section class="text-static">
+						
+							<p><small>Answers by Jigar Mehta (2014)</small></p>
+	
+							<p><strong>It’s been two years, where are you at?</strong> </p>
+	
+							<p>We bootstrapped, launched, iterated, failed, launched, iterated, failed, launched. Then collected, collected, collected. It was raw, it was real-time, and it was a new type of documentary storytelling. But  then the story we were covering changed, the climate changed. It felt like a natural “ending” point to our  activities. And now we’re at another pivotal point where it feels right to look back and reflect.</p>
+						</section>
+						
+						<h3>Q&amp;A</h3>
+						<section class="text-dynamic">
+
+							<p><small>Answers by Jigar Mehta (2014)</small></p>
+						
+							<p><strong>What’s one takeaway from the experience of bringing story and technology together?</strong></p>
+							<p>Technology is allowing us to shape the next generation of storytelling.</p>
+
+							<p>The objective of 18DaysInEgypt is to be the first living history documentary project created by the people who are living through it and experiencing it first-hand, using their own media. Egyptians documented a revolution in real-time using cellphones, cameras and social media such as Facebook and Twitter. There so many important stories out there about how the events played out in the streets of Egypt and how people were effected. We needed a way to collect all these stories and experiences in a crowd-sourced documentary.</p> 
+
+							<p>In a traditional documentary, we as filmmakers would choose the story to tell. But by using technology to drive the storytelling experience, we could create a new collaborative storytelling platform which would now enable an entire community to be the filmmakers. Everyone is a storyteller on <em>18DaysInEgypt</em>. </p>
+
+							<p>For the first time ever technology is now enabling an ongoing, evolving story. Egypt is still a country in transition and the site continues to build. Our community not only documented the first 18 days of the Egyptian revolution but the first presidential elections, ongoing clashes, new street art movements, rising political players and initiatives in the country. And the story still continues. That’s the power of using technology to drive storytelling - there are no boundaries, your imagination is the limit. It’s a very exciting time to be exploring in this new frontier.</p>
+
+							<p><strong>Has the experience developing your project given you new insights into ways to reach new audiences and or inspire social change?</strong></p>
+
+							<p>With a new form of crowd-sourced documentary, there are many new types of challenges to overcome that are unique to each project. Who are your audiences? How do you engage them? How do you reach them? How do you give your audience enough tools to properly experience your project? How do you teach your contributors new behaviors?</p>
+
+							<p>We learned that the most advantageous way to explore these questions was to be flexible, open to changes and pivots, and iterate quickly and often. There was no blueprint for what we were doing, and so we had to adopt some approaches from the tech-startup world in how we built out the project. We have gone through several iterations of the project, and we put everything out there to test out an idea and learn from the community about what was working and what wasn’t successful.</p>
+
+							<p>Another important lesson is to be aware of the context you are operating in. We built a project that has two main audiences: the Egyptian community and the larger global audience. In Egypt, there is a digital divide due to low-net penetration, approximately 25% of the population is online. We were very aware of this digital divide and our solution was creating an on-the-ground outreach effort which we called the 18DaysInEgypt fellows program. In the fellowship program, we hired and trained students and young citizen journalists who then went out and collected stories from the larger community. It was a very successful non-technical solution to engaging a community in Egypt that wouldn't normally have access to a web-native project.</p>
+						
+							<p>Creative solutions can lead to new paths and open doors in surprising ways leading to social change. The 18DaysInEgypt fellowship program was so successful it has now grown into a new citizen journalism incubator program called the Egypt Journalism Project which institutionalizes the education and training of young citizen journalists in Egypt. These young storytellers really are the future of Egypt and fulfill the original vision and mission of our project in a way we could never have imagined.</p>
+						
+						</section>
 					
 			
 					</section>
 				</article>
 			</li>
+			<li class="project anchor clearfix" id="testproject">
+				<article>
+					<header class="project-header" id="">
+						<a class="local-link" href="#testproject">
+							<div class="project-image-wrap">
+								<img src="img/content/prototype/18days.jpg" alt="Project Image">
+								<img src="img/content/prototype/18days.jpg" alt="18days">
+							</div>
+							<div class="header-info">
+								<div class="floating-text">
+								<h2>Test Project</h2>
+								<p> Sample Logline </p>			
+								</div>
+							</div>
+						</a>
+						<a class="project-url" href="http://18DaysinEgypt.com"target=_blank>18DaysinEgypt.com &rarr;</a>				
+					</header>
+					
+					<section class="project-content">
+					
+						<h3>Team Leaders</h3>
+						<ul class="team-leaders clearfix">
+							<li>
+								<div class="person-portrait">
+									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+								</div>
+								<div class="person-info">
+									<h4 class="name"> 
+										 <strong>JIGAR MEHTA</strong> 
+										 <br>
+										 Director/Producer/CO-Creator
+									</h4>
+									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University, where he worked on developing tools for collaborative journalism.</p>
+								</div>
+								
+							</li>
+							<li>
+								<div class="person-portrait">
+									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+								</div>
+								<div class="person-info">
+									<h4 class="name"> 
+										 <strong>YASMIN ELAYAT </strong> 
+										 <br>
+										 TECHNOLOGIST/CO-CREATOR
+									</h4>
+									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction designer and software developer based in Cairo.</p>
+								</div>
+								
+							</li>
+						</ul>		
+						
+						<h3>Project Update</h3>
+						<section class="text-static">
+						
+							<p><small>Answers by Jigar Mehta (2014)</small></p>
+	
+							<p><strong>It’s been two years, where are you at?</strong> </p>
+	
+							<p>We bootstrapped, launched, iterated, failed, launched, iterated, failed, launched. Then collected, collected, collected. It was raw, it was real-time, and it was a new type of documentary storytelling. But  then the story we were covering changed, the climate changed. It felt like a natural “ending” point to our  activities. And now we’re at another pivotal point where it feels right to look back and reflect.</p>
+						</section>
+						
+						<h3>Q&amp;A</h3>
+						<section class="text-dynamic">
+
+							<p><small>Answers by Jigar Mehta (2014)</small></p>
+						
+							<p><strong>What’s one takeaway from the experience of bringing story and technology together?</strong></p>
+							<p>Technology is allowing us to shape the next generation of storytelling.</p>
+
+							<p>The objective of 18DaysInEgypt is to be the first living history documentary project created by the people who are living through it and experiencing it first-hand, using their own media. Egyptians documented a revolution in real-time using cellphones, cameras and social media such as Facebook and Twitter. There so many important stories out there about how the events played out in the streets of Egypt and how people were effected. We needed a way to collect all these stories and experiences in a crowd-sourced documentary.</p> 
+
+							<p>In a traditional documentary, we as filmmakers would choose the story to tell. But by using technology to drive the storytelling experience, we could create a new collaborative storytelling platform which would now enable an entire community to be the filmmakers. Everyone is a storyteller on <em>18DaysInEgypt</em>. </p>
+
+							<p>For the first time ever technology is now enabling an ongoing, evolving story. Egypt is still a country in transition and the site continues to build. Our community not only documented the first 18 days of the Egyptian revolution but the first presidential elections, ongoing clashes, new street art movements, rising political players and initiatives in the country. And the story still continues. That’s the power of using technology to drive storytelling - there are no boundaries, your imagination is the limit. It’s a very exciting time to be exploring in this new frontier.</p>
+
+							<p><strong>Has the experience developing your project given you new insights into ways to reach new audiences and or inspire social change?</strong></p>
+
+							<p>With a new form of crowd-sourced documentary, there are many new types of challenges to overcome that are unique to each project. Who are your audiences? How do you engage them? How do you reach them? How do you give your audience enough tools to properly experience your project? How do you teach your contributors new behaviors?</p>
+
+							<p>We learned that the most advantageous way to explore these questions was to be flexible, open to changes and pivots, and iterate quickly and often. There was no blueprint for what we were doing, and so we had to adopt some approaches from the tech-startup world in how we built out the project. We have gone through several iterations of the project, and we put everything out there to test out an idea and learn from the community about what was working and what wasn’t successful.</p>
+
+							<p>Another important lesson is to be aware of the context you are operating in. We built a project that has two main audiences: the Egyptian community and the larger global audience. In Egypt, there is a digital divide due to low-net penetration, approximately 25% of the population is online. We were very aware of this digital divide and our solution was creating an on-the-ground outreach effort which we called the 18DaysInEgypt fellows program. In the fellowship program, we hired and trained students and young citizen journalists who then went out and collected stories from the larger community. It was a very successful non-technical solution to engaging a community in Egypt that wouldn't normally have access to a web-native project.</p>
+						
+							<p>Creative solutions can lead to new paths and open doors in surprising ways leading to social change. The 18DaysInEgypt fellowship program was so successful it has now grown into a new citizen journalism incubator program called the Egypt Journalism Project which institutionalizes the education and training of young citizen journalists in Egypt. These young storytellers really are the future of Egypt and fulfill the original vision and mission of our project in a way we could never have imagined.</p>
+						
+						</section>
+					
 			
+					</section>
+				</article>
+			</li>		
 		</ul>
 	
 		<section class="jury" id="">
