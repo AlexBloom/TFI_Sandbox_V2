e7a0e42556cb237723e3a30bd9df96e52b52bf98
Alex Bloom <alexbloomdesign@gmail.com>
2014-09-05 11:35:35 -0400

    Trashed Junk Files

--- tech-resources.php
+++ tech-resources.php
@@ -1,16 +1,16 @@
 <?php include_once 'inc/head.inc' ?>
 
 <!-- Custom Meta Info Here -->
-<link rel="canonical" href="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<link rel="canonical" href="<br><a href="" target=_blank>http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="article" />
 <meta property="og:title" content="TFI New Media Fund - 2014 Funded Projects - TFI Sandbox" />
 <meta property="og:description" content="The TFI New Media Fund is a grant for filmmakers pushing the boundaries of traditional filmmaking and documentary work." />
-<meta property="og:url" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
+<meta property="og:url" content="<br><a href="" target=_blank>http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014" />
 <meta property="og:site_name" content="TFI Sanxbox" />
 <meta property="article:published_time" content="2014-02-04T21:43:55+00:00" />
 <meta property="article:modified_time" content="2014-02-07T19:41:37+00:00" />
-<meta property="og:image" content="http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014/" />
+<meta property="og:image" content="<br><a href="" target=_blank>http://www.sandbox.tribecafilminstitute.org/tfi-new-media-fund/2014/" />
 
 <?php include_once 'inc/header.inc' ?>
 
@@ -43,27 +43,27 @@
 					<li>	
 						<h3>Zeega</h3>
 						Zeega is an open-source HTML5 platform for creating interactive documentaries and inventing new forms of storytelling
-						<a href="http://alpha.zeega.org"target=_blank> alpha.zeega.org </a>
+						<br><a href="<br><a href="" target=_blank>http://alpha.zeega.org"target=_blank> alpha.zeega.org </a>
 					</li>
 					<li>	
 						<h3>Mozilla PopcornMaker</h3>
 						Making it easy to enhance, remix and share web video
-						<a href="http://mozillapopcorn.org"target=_blank> mozillapopcorn.org </a>
+						<br><a href="<br><a href="" target=_blank>http://mozillapopcorn.org"target=_blank> mozillapopcorn.org </a>
 					</li>
 					<li>	
 						<h3>Storyplanet</h3>
 						Tool to create interactive projects
-						<a href="http://storyplanet.com/index"target=_blank> storyplanet.com/index </a>
+						<br><a href="<br><a href="" target=_blank>http://storyplanet.com/index"target=_blank> storyplanet.com/index </a>
 					</li>
 					<li>
 						<h3>Racontr</h3>
 						Create immersive stories without code.
-						<a href="https://beta.racontr.com/"target=_blank> beta.racontr.com </a>
+						<br><a href="<br><a href="" target=_blank>https://beta.racontr.com/"target=_blank> beta.racontr.com </a>
 					</li>
 					<li>
 						<h3>Kaltura</h3>
 						An Open Source Community providing tools such as: online video player, open source editor and open source video for websites.
-						<a href="http://corp.kaltura.com/"target=_blank> corp.kaltura.com </a>
+						<br><a href="<br><a href="" target=_blank>http://corp.kaltura.com/"target=_blank> corp.kaltura.com </a>
 					</li>
 				</ul>
 			</section>
@@ -73,93 +73,130 @@
 
 				<h3>Design Tools / Interface Prototyping / Before Code</h3>
 				<ul>	
-					<li>	Sketch - An affordable alternative to Adobe Creative Suite - http://bohemiancoding.com/sketch/
+					<li>	Sketch - An affordable alternative to Adobe Creative Suite - <br>
+						<a href="http://bohemiancoding.com/sketch/" target=_blank>http://bohemiancoding.com/sketch/</a>
 					</li>
-					<li>	Invision - http://www.invisionapp.com/
+					<li>	Invision - <br>
+						<a href="http://www.invisionapp.com/" target=_blank>http://www.invisionapp.com/</a>
 					</li>
 				</ul>
 				
 				<h3>Development Tools / Environments / Text Editors</h3>
 				<ul>	
-					<li>	TextMate - http://macromates.com/
+					<li>	TextMate - <br>
+						<a href="http://macromates.com/" target=_blank>http://macromates.com/</a>
 					</li>
-					<li>	Sublime Text - http://www.sublimetext.com/
+					<li>	Sublime Text - <br>
+						<a href="http://www.sublimetext.com/" target=_blank>http://www.sublimetext.com/</a>
 					</li>
-					<li>	Coda - https://panic.com/coda/
+					<li>	Coda - <br>
+						<a href="https://panic.com/coda/" target=_blank>https://panic.com/coda/</a>
 					</li>
-					<li>	GitHub &amp; Github for Mac - https://github.com/
+					<li>	GitHub &amp; Github for Mac - <br>
+						<a href="https://github.com/" target=_blank>https://github.com/</a>
 					</li>
-					<li>	Code Kit is my compiler and error checker. It's always running whenever I'm writing code - https://incident57.com/codekit/
+					<li>	Code Kit is my compiler and error checker. It's always running whenever I'm writing code - <br>
+						<a href="https://incident57.com/codekit/" target=_blank>https://incident57.com/codekit</a>
 					</li>
-					<li>	Transmit is my file transfer and server management software - http://panic.com/transmit/
+					<li>	Transmit is my file transfer and server management software - <br>
+						<a href="http://panic.com/transmit/" target=_blank>http://panic.com/transmit</a>
 					</li>
 				</ul>
 				
 				<h3>CODE 101 - Libraries, Resources, &amp; Instruction for Code</h3>
 				<ul>	
-					<li>	Initalizr - Virtually Every one of my web projects starts with this. - http://www.initializr.com/
+					<li>	Initalizr - Virtually Every one of my web projects starts with this. - <br>
+						<a href="http://www.initializr.com/" target=_blank>http://www.initializr.com/</a>
 					</li>
-					<li>	Dochub - A great search and reference sheet for when you forget what something means - http://dochub.io/
+					<li>	Dochub - A great search and reference sheet for when you forget what something means - <br>
+						<a href="http://dochub.io/" target=_blank>http://dochub.io/</a>
 					</li>
-					<li>	Pure CSS - Grab and Go responsive code patterns - http://purecss.io/
+					<li>	Pure CSS - Grab and Go responsive code patterns - <br>
+						<a href="http://purecss.io/" target=_blank>http://purecss.io/</a>
 					</li>
-					<li>	IcoMoon Icon Font Generator - http://icomoon.io/
+					<li>	IcoMoon Icon Font Generator - <br>
+						<a href="http://icomoon.io/" target=_blank>http://icomoon.io/</a>
 					</li>
-					<li>	Fontello - Icon Font Generator - http://fontello.com/
+					<li>	Fontello - Icon Font Generator - <br>
+						<a href="http://fontello.com/" target=_blank>http://fontello.com/</a>
 					</li>
-					<li>	Google Fonts Free Webfont Resource - http://www.google.com/fonts/#
+					<li>	Google Fonts Free Webfont Resource - <br>
+						<a href="http://www.google.com/fonts/#" target=_blank>http://www.google.com/fonts/#</a>
 					</li>
-					<li>	Typekit Affordable Webfont Library - http://www.typekit.com
+					<li>	Typekit - Affordable Webfont Library - <br>
+						<a href="http://www.typekit.com" target=_blank>http://www.typekit.com</a>
 					</li>
-					<li>	SASS and LESS - Once someone has an understanding of CSS, these are invaluable for making development easier and faster: http://sass-lang.com/ , and http://lesscss.org/
+					<li>	SASS and LESS - Once someone has an understanding of CSS, these are invaluable for making development easier and faster: <br>
+						<a href=http://sass-lang.com/"" target=_blank>http://sass-lang.com/</a> , and <br>
+						<a href="http://lesscss.org/" target=_blank>http://lesscss.org/</a>
 					</li>
-					<li>	Modernizr - A javascript library to help improve visual consistency across browsers - http://modernizr.com/
-				Web -  Educational / Social Coding Sites
-					</li>
-					<li>	CodePen and JS Fiddle - Quickly test things in the browser and share them for help or advice, great forums - http://codepen.io/ and  http://jsfiddle.net/
-					</li>
-					<li>	Code Academy - Free Online School - http://www.codecademy.com/
-					</li>
-					<li>	Code School &amp; TreeHouse - Affordable Online Schools - https://www.codeschool.com/ and http://teamtreehouse.com/
+					<li>	Modernizr - A javascript library to help improve visual consistency across browsers - <br>
+						<a href="http://modernizr.com/" target=_blank>http://modernizr.com/</a>
 					</li>
 				</ul>
 				
+				<h3>Web -  Educational / Social Coding Sites</h3>
+				<ul>
+					<li>	CodePen and JS Fiddle - Quickly test things in the browser and share them for help or advice, great forums - <br>
+						<a href="http://codepen.io/" target=_blank>http://codepen.io/</a> and  <br>
+						<a href="http://jsfiddle.net/" target=_blank>http://jsfiddle.net/</a>
+					</li>
+					<li>	Code Academy - Free Online School - <br>
+						<a href="http://www.codecademy.com/" target=_blank>http://www.codecademy.com/</a>
+					</li>
+					<li>	Code School &amp; TreeHouse - Affordable Online Schools - <br>
+						<a href="https://www.codeschool.com/" target=_blank>https://www.codeschool.com/</a> and <br>
+						<a href="http://teamtreehouse.com/" target=_blank>http://teamtreehouse.com/</a>
+					</li>
+				</ul>
 				
 				<h3>Web - Back End Development </h3>
 				<ul>
-					<li>	PHP - Possibly the most well established server side language - http://www.php.net/
+					<li>	PHP - Possibly the most well established server side language - <br>
+						<a href="http://www.php.net/" target=_blank>http://www.php.net/</a>
 					</li>
-					<li>	Ruby on Rails - Quick learning curve, extremely popular with web based applications - http://rubyonrails.org/
+					<li>	Ruby on Rails - Quick learning curve, extremely popular with web based applications - <br>
+						<a href="http://rubyonrails.org/" target=_blank>http://rubyonrails.org/</a>
 					</li>
-					<li>	Jekyll - Static Site generator that runs on ruby, stable, secure, and fast - http://jekyllrb.com/
+					<li>	Jekyll - Static Site generator that runs on ruby, stable, secure, and fast - <br>
+						<a href="http://jekyllrb.com/" target=_blank>http://jekyllrb.com/</a>
 					</li>
-					<li>	Liquid - Shopify and Siteleaf use this templating language, very easy to learn the syntax - http://liquidmarkup.org/
+					<li>	Liquid - Shopify and Siteleaf use this templating language, very easy to learn the syntax - <br>
+						<a href="http://liquidmarkup.org/" target=_blank>http://liquidmarkup.org/</a>
 					</li>
 				</ul>
 				
 				<h3>Web - Content Management Systems</h3>
 				<ul>
-					<li>	Wordpress - The Old Standard PHP / Database CMS, The largest community of open source developers on the web - http://wordpress.org
+					<li>	Wordpress - The Old Standard PHP / Database CMS, The largest community of open source developers on the web - <br>
+						<a href="http://wordpress.org" target=_blank>http://wordpress.org</a>
 					</li>
-					<li>	Kirby - PHP / File Based CMS - Can run out of dropbox - http://getkirby.com/
+					<li>	Kirby - PHP / File Based CMS - Can run out of dropbox - <br>
+						<a href="http://getkirby.com" target=_blank>http://getkirby.com/</a>
 					</li>
-					<li>	SiteLeaf - Jekyll Based CMS - Affordable plans, easily customizable - http://www.siteleaf.com/
+					<li>	SiteLeaf - Jekyll Based CMS - Affordable plans, easily customizable - <br>
+						<a href=">http://www.siteleaf.com/" target=_blank>http://www.siteleaf.com/</a>
 					</li>
 				</ul>
 				
 				<h3>Data Visualization / Multi-Component / Projection Mapping Software</h3>
 				<ul>
-					<li>	Processing - http://www.processing.org/
+					<li>	Processing - <br>
+						<a href="http://www.processing.org/" target=_blank>http://www.processing.org/</a>
 					</li>
-					<li>	Cinder Frameworks - http://libcinder.org/
+					<li>	Cinder Frameworks - <br>
+						<a href="http://libcinder.org/" target=_blank>http://libcinder.org/</a>
 					</li>
 				</ul>
+				
 				<h3>DIY / Multi-Component / Projection Mapping Hardware </h3>
 				<ul>
 					</li>
-					<li>	Arduino - Awesome open source computer platform & hardware - http://www.arduino.cc/
+					<li>	Arduino - Awesome open source computer platform &amp; hardware - <br>
+						<a href="http://www.arduino.cc/" target=_blank>http://www.arduino.cc/</a>
 					</li>
-					<li>	Brightsign - Good, affordable media players -  http://www.brightsign.biz/
+					<li>	Brightsign - Good, affordable media players -  <br>
+						<a href="http://www.brightsign.biz/" target=_blank>http://www.brightsign.biz/
 					</li>
 				</ul>
 				
