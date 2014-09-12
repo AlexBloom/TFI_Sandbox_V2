5a48019b32040427164932afa25ea57f359a7e12
Daniel Su <dsu@tribecafilminstitute.org>
2014-09-12 12:01:17 -0400

    nav switch to GET vars, DB integration, and unicode (smart quotesa and foreign words)

--- tfi-new-media-fund-projects.php
+++ tfi-new-media-fund-projects.php
@@ -1,8 +1,12 @@
-<?php include_once 'inc/head.inc' ?>
 <?php
+include_once 'inc/head.inc';
 require_once 'inc/tfi_ee.inc';
+
+$vars = array_keys($_GET);
+$current_year = $vars[0];
 $cms = new tfi_ee();
-$projects = $cms->get_project_by_year(2014);
+$projects = $cms->get_project_by_year($current_year);
+
 ?>
 
 <!-- Custom Meta Info Here -->
@@ -26,11 +30,11 @@ $projects = $cms->get_project_by_year(2014);
 		<li>
 			<dl class="tertiary">
 				<!-- Enter Current Page Name as DT Item Here -->
-				<dt> 2014 <span class="icon-down"> <span> </dt>
-				<dd><a href="tfi-new-media-fund-2014"> 2014 </a> </dd>
-				<dd><a href="tfi-new-media-fund-2013"> 2013 </a> </dd>
-				<dd><a href="tfi-new-media-fund-2012"> 2012 </a> </dd>
-				<dd><a href="tfi-new-media-fund-2011"> 2011 </a> </dd>
+				<dt> <?=$current_year?> <span class="icon-down"> <span> </dt>
+				<dd><a href="tfi-new-media-fund-projects?2014"> 2014 </a> </dd>
+				<dd><a href="tfi-new-media-fund-projects?2013"> 2013 </a> </dd>
+				<dd><a href="tfi-new-media-fund-projects?2012"> 2012 </a> </dd>
+				<dd><a href="tfi-new-media-fund-projects?2011"> 2011 </a> </dd>
 			<dl>
 		</li>
 	</ul>
@@ -44,25 +48,25 @@ $projects = $cms->get_project_by_year(2014);
 	<header>
 		<h1>TFI New Media Fund</h1>
 		
-		<p class="intro">2014 Grantees and Jurors</p>	
+		<p class="intro"><?=$current_year?> Grantees and Jurors</p>	
 		
 	</header>
 </article>
-	<section class="clearfix" id="2014">		
+	<section class="clearfix" id="<?=$current_year?>">		
 		
 		<ul class="projects">
-			<li class="project anchor clearfix" id="18Days">
+			<?php foreach($projects as $project){ ?>
+			<li class="project anchor clearfix" id="project_<?=$project['url_title']?>">
 				<article>
 					<header class="project-header" id="">
-						<a class="local-link" href="#18Days">
+						<a class="local-link" href="#project_<?=$project['url_title']?>">
 							<div class="project-image-wrap">
-								<img src="img/content/prototype/18days.jpg" alt="Project Image">
-								<img src="img/content/prototype/18days.jpg" alt="18days">
+								<img src="<?=str_replace('{filedir_3}', 'https://tribecafilminstitute.org/images/uploads/film_files/', $project['film_image'])?>" alt="Project Image">
 							</div>
 							<div class="header-info">
 								<div class="floating-text">
-								<h2>18 Days in Egypt</h2>
-								<p> 18DaysinEgypt.com is an interactive documentary project that empowers Egyptians to preserve the incredible social media they created--their on-the-ground, eye-witness accounts--and to add their voice to an important story. </p>			
+								<h2><?=$project['title']?></h2>
+								<?=$project['film_logline']?>
 								</div>
 							</div>
 						</a>
@@ -73,34 +77,21 @@ $projects = $cms->get_project_by_year(2014);
 					
 						<h3>Team Leaders</h3>
 						<ul class="team-leaders clearfix">
+							<?php foreach($project['filmmakers'] as $filmmaker){ ?>
 							<li>
 								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+									<img src="<?=str_replace('{filedir_4}', 'https://tribecafilminstitute.org/images/uploads/filmmaker_files/', $filmmaker['ch_filmmaker_headshot'])?>" alt="Michele Stephenson Portrait"/>
 								</div>
 								<div class="person-info">
 									<h4 class="name"> 
-										 <strong>JIGAR MEHTA</strong> 
-										 <br>
-										 Director/Producer/CO-Creator
+										 <strong><?=$filmmaker['ch_filmmaker_fname'].' '.$filmmaker['ch_filmmaker_lname']?></strong> 
+										 <br><?=$filmmaker['mx_filmmakers_role']?>
 									</h4>
-									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University, where he worked on developing tools for collaborative journalism.</p>
+									<?=$filmmaker['ch_filmmaker_bio']?>
 								</div>
 								
 							</li>
-							<li>
-								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-								</div>
-								<div class="person-info">
-									<h4 class="name"> 
-										 <strong>YASMIN ELAYAT </strong> 
-										 <br>
-										 TECHNOLOGIST/CO-CREATOR
-									</h4>
-									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction designer and software developer based in Cairo.</p>
-								</div>
-								
-							</li>
+							<?php }?>
 						</ul>		
 						
 						<h3>Project Update</h3>
@@ -143,121 +134,113 @@ $projects = $cms->get_project_by_year(2014);
 					</section>
 				</article>
 			</li>
-			<li class="project anchor clearfix" id="testproject">
-				<article>
-					<header class="project-header" id="">
-						<a class="local-link" href="#testproject">
-							<div class="project-image-wrap">
-								<img src="img/content/prototype/18days.jpg" alt="Project Image">
-								<img src="img/content/prototype/18days.jpg" alt="18days">
-							</div>
-							<div class="header-info">
-								<div class="floating-text">
-								<h2>Test Project</h2>
-								<p> Sample Logline </p>			
-								</div>
-							</div>
-						</a>
-						<a class="project-url" href="http://18DaysinEgypt.com"target=_blank>18DaysinEgypt.com &rarr;</a>				
-					</header>
-					
-					<section class="project-content">
-					
-						<h3>Team Leaders</h3>
-						<ul class="team-leaders clearfix">
-							<li>
-								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-								</div>
-								<div class="person-info">
-									<h4 class="name"> 
-										 <strong>JIGAR MEHTA</strong> 
-										 <br>
-										 Director/Producer/CO-Creator
-									</h4>
-									<p> Jigar Mehta is responsible for developing and managing the project’s story content. Jigar is a documentary filmmaker and a journalist. He was a John S. Knight Fellow at Stanford University, where he worked on developing tools for collaborative journalism.</p>
-								</div>
-								
-							</li>
-							<li>
-								<div class="person-portrait">
-									<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-								</div>
-								<div class="person-info">
-									<h4 class="name"> 
-										 <strong>YASMIN ELAYAT </strong> 
-										 <br>
-										 TECHNOLOGIST/CO-CREATOR
-									</h4>
-									<p> Yasmin Elayat is the lead technologist for the project, primarily responsible for developing the design &amp; vision for 18DaysinEgypt.com and leading the development team. Yasmin is an interaction designer and software developer based in Cairo.</p>
-								</div>
-								
-							</li>
-						</ul>		
-						
-						<h3>Project Update</h3>
-						<section class="text-static">
-						
-							<p><small>Answers by Jigar Mehta (2014)</small></p>
-	
-							<p><strong>It’s been two years, where are you at?</strong> </p>
-	
-							<p>We bootstrapped, launched, iterated, failed, launched, iterated, failed, launched. Then collected, collected, collected. It was raw, it was real-time, and it was a new type of documentary storytelling. But  then the story we were covering changed, the climate changed. It felt like a natural “ending” point to our  activities. And now we’re at another pivotal point where it feels right to look back and reflect.</p>
-						</section>
-						
-						<h3>Q&amp;A</h3>
-						<section class="text-dynamic">
-
-							<p><small>Answers by Jigar Mehta (2014)</small></p>
-						
-							<p><strong>What’s one takeaway from the experience of bringing story and technology together?</strong></p>
-							<p>Technology is allowing us to shape the next generation of storytelling.</p>
-
-							<p>The objective of 18DaysInEgypt is to be the first living history documentary project created by the people who are living through it and experiencing it first-hand, using their own media. Egyptians documented a revolution in real-time using cellphones, cameras and social media such as Facebook and Twitter. There so many important stories out there about how the events played out in the streets of Egypt and how people were effected. We needed a way to collect all these stories and experiences in a crowd-sourced documentary.</p> 
-
-							<p>In a traditional documentary, we as filmmakers would choose the story to tell. But by using technology to drive the storytelling experience, we could create a new collaborative storytelling platform which would now enable an entire community to be the filmmakers. Everyone is a storyteller on <em>18DaysInEgypt</em>. </p>
-
-							<p>For the first time ever technology is now enabling an ongoing, evolving story. Egypt is still a country in transition and the site continues to build. Our community not only documented the first 18 days of the Egyptian revolution but the first presidential elections, ongoing clashes, new street art movements, rising political players and initiatives in the country. And the story still continues. That’s the power of using technology to drive storytelling - there are no boundaries, your imagination is the limit. It’s a very exciting time to be exploring in this new frontier.</p>
-
-							<p><strong>Has the experience developing your project given you new insights into ways to reach new audiences and or inspire social change?</strong></p>
-
-							<p>With a new form of crowd-sourced documentary, there are many new types of challenges to overcome that are unique to each project. Who are your audiences? How do you engage them? How do you reach them? How do you give your audience enough tools to properly experience your project? How do you teach your contributors new behaviors?</p>
-
-							<p>We learned that the most advantageous way to explore these questions was to be flexible, open to changes and pivots, and iterate quickly and often. There was no blueprint for what we were doing, and so we had to adopt some approaches from the tech-startup world in how we built out the project. We have gone through several iterations of the project, and we put everything out there to test out an idea and learn from the community about what was working and what wasn’t successful.</p>
-
-							<p>Another important lesson is to be aware of the context you are operating in. We built a project that has two main audiences: the Egyptian community and the larger global audience. In Egypt, there is a digital divide due to low-net penetration, approximately 25% of the population is online. We were very aware of this digital divide and our solution was creating an on-the-ground outreach effort which we called the 18DaysInEgypt fellows program. In the fellowship program, we hired and trained students and young citizen journalists who then went out and collected stories from the larger community. It was a very successful non-technical solution to engaging a community in Egypt that wouldn't normally have access to a web-native project.</p>
-						
-							<p>Creative solutions can lead to new paths and open doors in surprising ways leading to social change. The 18DaysInEgypt fellowship program was so successful it has now grown into a new citizen journalism incubator program called the Egypt Journalism Project which institutionalizes the education and training of young citizen journalists in Egypt. These young storytellers really are the future of Egypt and fulfill the original vision and mission of our project in a way we could never have imagined.</p>
-						
-						</section>
-					
-			
-					</section>
-				</article>
-			</li>		
+			<?php } ?>
 		</ul>
-	
-		<section class="jury" id="">
-			<h2>Meet The Jury</h2>
-			<ul class="jury-members clearfix">
-				<li>
-					<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
-					<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
-				</li>
-				<li>
-					<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
-					<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
-				</li>
-				<li>
-					<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
-					<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
-				</li>
-				<li>
-					<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
-					<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
-				</li>
-			</ul>
-		</section>
+		
+		<?php
+		switch($current_year) {
+			case '2014':
+				?>
+				<section class="jury" id="">
+					<h2>Meet The Jury</h2>
+					<ul class="jury-members clearfix">
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+					</ul>
+				</section>
+				<?php
+				break;
+			case '2013':
+				?>
+				<section class="jury" id="">
+					<h2>Meet The Jury</h2>
+					<ul class="jury-members clearfix">
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+					</ul>
+				</section>
+				<?php
+				break;
+			case '2012':
+				?>
+				<section class="jury" id="">
+					<h2>Meet The Jury</h2>
+					<ul class="jury-members clearfix">
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+					</ul>
+				</section>
+				<?php
+				break;
+			case '2011':
+				?>
+				<section class="jury" id="">
+					<h2>Meet The Jury</h2>
+					<ul class="jury-members clearfix">
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Michele Stephenson Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br> Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong>  <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+						<li>
+							<img src="img/content/prototype/person-image.jpg" alt="Joe Brewster Portrait"/>
+							<span class="name"> <strong> Firstname Lastname</strong> <br>  Title / Accolades </span>
+						</li>
+					</ul>
+				</section>
+				<?php
+				break;
+		}
+		?>
 		
 		<nav class="share">
 			<div id="share-engage"> <span class="icon icon-share"> </span> SHARE  </div>
